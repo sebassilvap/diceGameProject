7c6b6d181ac98a49eff95a7aac887b204931e92b
César Sebastián Silva Proaño <39182965+sebassilvap@users.noreply.github.com>
2022-03-14 12:17:07 +0100

    Correction in the JavaScript Code

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ if (randomNumber1 === 1) {
 
 if (randomNumber2 === 1) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
-} else if (randomNumber1 === 2) {
+} else if (randomNumber2 === 2) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
-} else if (randomNumber1 === 3) {
+} else if (randomNumber2 === 3) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png");
-} else if (randomNumber1 === 4) {
+} else if (randomNumber2 === 4) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
-} else if (randomNumber1 === 5) {
+} else if (randomNumber2 === 5) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
-} else if (randomNumber1 === 6) {
+} else if (randomNumber2 === 6) {
   document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
 }
 
